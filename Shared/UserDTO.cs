90e1ce34404fab642314842f89b346a037eaa696
Patrick N <paab@itu.dk>
2023-02-20 16:06:45 +0100

    DTO cleanup

--- Shared/UserDTO.cs
+++ Shared/UserDTO.cs
@@ -1,14 +1,6 @@
 namespace MiniTwit.Shared
 {
-    public record UserDTO
-    {
-        public string Username { get; set; }
-
-        public string Password { get; set; }
-
-        public string Email { get; set; }
-    }
-
+    public record UserDTO(string Username, string Email, string Password);
     public record UserLoginDTO(string Username, string Password);
 }
 
