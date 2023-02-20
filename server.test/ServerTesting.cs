08cd5ab13766af9e97d052ab7a14beab4e764ba8
Patrick N <paab@itu.dk>
2023-02-20 15:25:16 +0100

    add testing framework using EFC for API

--- server.test/ServerTesting.cs
+++ server.test/ServerTesting.cs
@@ -1,9 +1,7 @@
 namespace server.test;
 
 using System.Text;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.Mvc;
-using MiniTwit.Shared;
 using Newtonsoft.Json;
 
 /* 
@@ -18,120 +16,41 @@ using Newtonsoft.Json;
 
  */
 
-public class server_testing : IClassFixture<WebApplicationFactory<Program>>
+public class server_testing : IClassFixture<WebTestFixture>
 {
-    private readonly WebApplicationFactory<Program> _factory;
-
-    public server_testing(WebApplicationFactory<Program> factory)
+    private readonly HttpClient _client;
+    public server_testing(WebTestFixture factory)
     {
-
-        // Before each test, set up a blank database in sqlite memory
-        // SqliteConnection db = new SqliteConnection("Data Source=:memory:");
-        _factory = factory;
-
-
+        _client = factory.CreateClient();
     }
-
-    // helper functions
-    private async Task<HttpResponseMessage> Register(string username, string password, string? email = null)
-    {
-        /*         if (password2 == null)
-                    password2 = password; */
-        var _client = _factory.CreateClient();
-        if (email == null)
-            email = username + "@example.com";
-        var json = JsonConvert.SerializeObject(new UserDTO { Username = username, Email = email, Password = password });
-        Console.WriteLine(json);
-        var content = new StringContent(json.ToString(), System.Text.Encoding.UTF8, "application/json");
-        var resp = _client.PostAsync("/minitwit/register", content);
-        return await resp;
-    }
-
-    public async Task<string> GetContent(HttpResponseMessage response)
-    {
-        var encoding = Encoding.UTF8;
-        var content = response.Content;
-
-        var contentBytes = await content.ReadAsByteArrayAsync();
-        return encoding.GetString(contentBytes);
-    }
-    /*     private async Task<HttpResponseMessage> Login(string username, string password)
-        {
-            var content = new StringContent(JsonSerializer.Serialize(new
-            {
-                username = username,
-                password = password
-            }), Encoding.UTF8, "application/json");
-
-            return await _client.PostAsync("/login", content);
-        }
-     */
-    /*     private async Task<HttpResponseMessage> RegisterAndLogin(string username, string password)
-        {
-            await Register(username, password);
-            return await Login(username, password);
-        }
-     */
-    private async Task<HttpResponseMessage> Logout()
-    {
-        var _client = _factory.CreateClient();
-
-        return await _client.GetAsync("/logout");
-    }
-
-    private async Task<HttpResponseMessage> AddMessage(string text)
-    {
-        var _client = _factory.CreateClient();
-
-        var json = JsonConvert.SerializeObject(new Message
-        {
-            AuthorId = 0,
-            Text = text,
-            PubDate = DateTime.Now,
-            Flagged = 0
-        });
-        // Console.WriteLine(json);
-        var content = new StringContent(json.ToString(), System.Text.Encoding.UTF8, "application/json");
-
-        var response = await _client.PostAsync("/add_message", content);
-
-        return response;
-    }
-
 
     [Fact]
     public async void TestSimpleRegister()
     {
         var response = await Register("user1", "default");
-        response.Should().BeOfType<CreatedResult>();
+        response.Should().BeSuccessful();
     }
 
-    [Fact(Skip = "it dont work")]
+    [Fact]
     public async void TestRegister()
     {
         var response = await Register("user1", "default");
-        var responseContent = response.Content.ReadAsStringAsync().Result;
-        Assert.Equal("You were successfully registered and can login now", responseContent);
+        response.Should().BeSuccessful();
 
         response = await Register("user1", "default");
-        responseContent = response.Content.ReadAsStringAsync().Result;
-        Assert.Equal("The username is already taken", responseContent);
+        response.Should().HaveClientError();
 
-        response = await Register("", "default");
-        responseContent = response.Content.ReadAsStringAsync().Result;
-        Assert.Equal("You have to enter a username", responseContent);
+        // response = await Register("", "default");
+        // Assert.Equal("You have to enter a username", responseContent);
 
-        response = await Register("meh", "");
-        responseContent = response.Content.ReadAsStringAsync().Result;
-        Assert.Equal("You have to enter a password", responseContent);
+        // response = await Register("meh", "");
+        // Assert.Equal("You have to enter a password", responseContent);
 
-        response = await Register("meh", "x", "y");
-        responseContent = response.Content.ReadAsStringAsync().Result;
-        Assert.Equal("The two passwords do not match", responseContent);
+        //response = await Register("meh", "x", "y");
+        //Assert.Equal("The two passwords do not match", responseContent);
 
-        response = await Register("meh", "foo", email: "broken");
-        responseContent = response.Content.ReadAsStringAsync().Result;
-        Assert.Equal("You have to enter a valid email address", responseContent);
+        //response = await Register("meh", "foo", email: "broken");
+        //Assert.Equal("You have to enter a valid email address", responseContent);
     }
     /*     [Fact]
         public async Task LoginLogoutTest()
@@ -212,4 +131,56 @@ public class server_testing : IClassFixture<WebApplicationFactory<Program>>
         Assert.DoesNotContain("the message by foo", content);
         Assert.Contains("the message by bar", content);
     } */
+
+    // helper functions
+    private async Task<HttpResponseMessage> Register(string username, string password, string? email = null)
+    {
+        if (email == null) email = username + "@example.com";
+        var content = stringify_object(new UserDTO(username, email, password));
+        var resp = await _client.PostAsync("minitwit/register", content);
+        return resp;
+    }
+
+    private async Task<HttpResponseMessage> Login(string username, string password)
+    {
+        var content = stringify_object(new UserDTO(username, "", password));
+        return await _client.PostAsync("minitwit/login", content);
+    }
+
+    private async Task<HttpResponseMessage> RegisterAndLogin(string username, string password)
+    {
+        await Register(username, password);
+        return await Login(username, password);
+    }
+
+    private async Task<HttpResponseMessage> Logout()
+    {
+        return await _client.GetAsync("minitwit/logout");
+    }
+
+    private StringContent stringify_object(Object ob)
+    {
+        return new StringContent(
+                    JsonConvert.SerializeObject(ob).ToString(),
+                    Encoding.UTF8,
+                    "application/json"
+        );
+    }
+
+    private async Task<HttpResponseMessage> AddMessage(string text)
+    {
+        var json = JsonConvert.SerializeObject(new Message
+        {
+            AuthorId = 0,
+            Text = text,
+            PubDate = DateTime.Now,
+            Flagged = 0
+        });
+        // Console.WriteLine(json);
+        var content = new StringContent(json.ToString(), System.Text.Encoding.UTF8, "application/json");
+
+        var response = await _client.PostAsync("minitwit/add_message", content);
+
+        return response;
+    }
 }
---
+++ server.test/WebTestFixture.cs
@@ -0,0 +1,50 @@
+namespace server.test;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MiniTwit.Server;
+using System;
+
+public class WebTestFixture : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+
+        //   builder.UseEnvironment("Testing");
+
+
+        builder.ConfigureServices(services =>
+        {
+            // Replace the database context registration with an in-memory database context.
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbContextOptions<TwitContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<TwitContext>(options =>
+            {
+                options.UseInMemoryDatabase("TestDb");
+            });
+
+            // Build the service provider.
+            var sp = services.BuildServiceProvider();
+
+            // Create a scope to obtain a reference to the database context
+            using (var scope = sp.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<TwitContext>();
+
+                // Ensure the database is created.
+                db.Database.EnsureCreated();
+            }
+        });
+    }
+}
