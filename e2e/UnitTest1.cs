df5e0176ae829c22d944adb8b953f924a2bda6e1
silasarildsen1 <silas.r.arildsen@gmail.com>
2023-03-10 13:18:06 +0100

    Fix Playwright

---
+++ e2e/UnitTest1.cs
@@ -0,0 +1,36 @@
+namespace e2e;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class SignInTest : PageTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+
+    [Test]
+    public async Task TestSignIn()
+    {
+        await Page.GotoAsync("http://localhost:5235/public");
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "sign in" }).ClickAsync();
+
+        await Page.Locator("input[name=\"username\"]").ClickAsync();
+
+        await Page.Locator("input[name=\"username\"]").FillAsync("asdasd");
+
+        await Page.Locator("input[name=\"username\"]").PressAsync("Tab");
+
+        await Page.Locator("input[name=\"password\"]").FillAsync("asdasd");
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Sign In" }).ClickAsync();
+
+        Page.GetByRole(AriaRole.Link, new() { Name = "sign out [asdasd]" }).Should().NotBeNull();
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "sign out [asdasd]" }).ClickAsync();
+
+        Page.GetByRole(AriaRole.Link, new() { Name = "sign in" }).Should().NotBeNull();
+    }
+}
--- e2e/Usings.cs
+++ e2e/Usings.cs
@@ -1,4 +1,4 @@
-global using Xunit;
-global using FluentAssertions;
+global using NUnit.Framework;
 global using Microsoft.Playwright;
-global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.Playwright.NUnit;
+global using FluentAssertions;
