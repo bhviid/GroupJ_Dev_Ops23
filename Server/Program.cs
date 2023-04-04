a5e986e449e03e1b7eec895cb0c6445452d481c1
tjobi <tobias@brund.info>
2023-04-04 13:44:18 +0200

    seed db with random data for development

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MiniTwit.Shared;
 using Microsoft.EntityFrameworkCore.InMemory;
-    using Serilog;
+using Serilog;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
@@ -9,74 +9,79 @@ Log.Logger = new LoggerConfiguration()
 
 
 try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Configuration.AddEnvironmentVariables(prefix: "connection_string");
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddRazorPages();
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration));
+
+    if (builder.Environment.IsDevelopment())
     {
+        Console.WriteLine("Starting Development database");
+        builder.Services.AddDbContext<TwitContext>(options =>
+            options.UseInMemoryDatabase(databaseName: "SlimTwit"));
 
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Configuration.AddEnvironmentVariables(prefix: "connection_string");
-        builder.Services.AddControllersWithViews();
-        builder.Services.AddRazorPages();
-        builder.Host.UseSerilog((context, services, configuration) => configuration
-            .ReadFrom.Configuration(context.Configuration));
-
-        if (builder.Environment.IsDevelopment())
-        {
-            Console.WriteLine("Starting Development database");
-            builder.Services.AddDbContext<TwitContext>(options =>
-                options.UseInMemoryDatabase(databaseName: "SlimTwit"));
-
-        }
-        else
-        {
-            builder.Services.AddDbContext<TwitContext>(options =>
-                options.UseNpgsql(Environment.GetEnvironmentVariable("connection_string")));
-        }
-
-        var app = builder.Build();
-
-
-// Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseWebAssemblyDebugging();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        using var scope = app.Services.CreateScope();
-        var context = scope.ServiceProvider.GetRequiredService<TwitContext>();
-        if (!context.Database.IsInMemory() && context.Database.GetPendingMigrations().Any())
-        {
-            context.Database.Migrate();
-        }
-
-        context.Database.EnsureCreated();
-
-        app.UseHttpsRedirection();
-
-        app.UseBlazorFrameworkFiles();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.MapRazorPages();
-        app.MapControllers();
-        app.MapFallbackToFile("index.html");
-        app.UseSerilogRequestLogging();
-        app.Run();
     }
-    catch (Exception ex)
+    else
     {
-        Log.Fatal(ex, "Application terminated unexpectedly!");
-    }
-    finally
-    {
-        Log.CloseAndFlush();
+        builder.Services.AddDbContext<TwitContext>(options =>
+            options.UseNpgsql(Environment.GetEnvironmentVariable("connection_string")));
     }
 
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseWebAssemblyDebugging();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<TwitContext>();
+    if (!context.Database.IsInMemory() && context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+    //Safe to seed here?
+    if(args.Length > 0 && args[0] == "seed")
+    {
+        Console.WriteLine("attempting to seed");
+        context.InitFakeData();
+        Console.WriteLine("attempting to seed");
+    }
+
+    context.Database.EnsureCreated();
+
+    app.UseHttpsRedirection();
+
+    app.UseBlazorFrameworkFiles();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.MapRazorPages();
+    app.MapControllers();
+    app.MapFallbackToFile("index.html");
+    app.UseSerilogRequestLogging();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly!");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
 
-    public partial class Program { }
+
+public partial class Program { }
