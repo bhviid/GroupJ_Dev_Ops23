e78965476c4c282b4758010221ea889d401e9f64
Patrick N <paab@itu.dk>
2023-05-02 14:08:43 +0200

    Co-authored-by: tjobi <tjobi@users.noreply.github.com> bhviid <bhviid@users.noreply.github.com> silas <silasarildsen1@users.noreply.github.com>

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,11 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
 }
-
 
 
 # Set the variable value in *.tfvars file
@@ -15,9 +14,15 @@ variable "do_token" {}
 variable "pub_key" {}
 variable "pvt_key" {}
 
+provider "digitalocean" { token = var.do_token }
+
 resource "digitalocean_ssh_key" "minitwit" {
-    name = "tf"
-    public_key = file(var.pub_key)
+  name       = "tf"
+  public_key = file(var.pub_key)
+}
+
+resource "digitalocean_tag" "taerteform" {
+  name = "taerteform"
 }
 
 # Configure the DigitalOcean Provider
@@ -37,24 +42,25 @@ resource "digitalocean_ssh_key" "minitwit" {
 
 # Simulation API server Swarm manager
 resource "digitalocean_droplet" "slimtwit-swarm-manager" {
-  name = "sim-swarm-manager"
-  image = "docker-18-04"
+  name   = "sim-swarm-manager"
+  image  = "docker-18-04"
   region = "fra1"
   size   = "s-1vcpu-1gb"
+  tags   = [digitalocean_tag.taerteform.id]
 
   connection {
-    user = "root"
-    host = self.ipv4_address
-    type = "ssh"
+    user        = "root"
+    host        = self.ipv4_address
+    type        = "ssh"
     private_key = file(var.pvt_key)
-    timeout = "2m"
+    timeout     = "2m"
   }
 
   # what this?
-  ssh_keys = [ digitalocean_ssh_key.minitwit.fingerprint ]
+  ssh_keys = [digitalocean_ssh_key.minitwit.fingerprint]
 
   provisioner "file" {
-    source = "../Docker/prod-slimtwit-compose/docker-compose.yml"
+    source      = "../Docker/prod-slimtwit-compose/docker-compose.yml"
     destination = "~/docker-compose.yml"
   }
 
@@ -80,7 +86,7 @@ resource "digitalocean_droplet" "slimtwit-swarm-manager" {
   # GET THE WORKER JOIN TOKEN FROM THE NEWLY CREATED SWARM MANAGER NODE
   provisioner "local-exec" {
     command = <<EOS
-      ssh -o 'StrictHostKeyCheckingno' root@${self.ipv4_address} -i ssh_key/terraform
+      ssh -o 'StrictHostKeyChecking=no' root@${self.ipv4_address} -i ssh_key/terraform
       'docker swarm join-token worker -q' > temp/worker_token
     EOS
   }
@@ -88,30 +94,30 @@ resource "digitalocean_droplet" "slimtwit-swarm-manager" {
 
 # Simulation API server Swarm nodes
 resource "digitalocean_droplet" "slimtwit" {
-  depends_on = [ digitalocean_droplet.slimtwit-swarm-manager ]
-  image = "docker-18-04"
-  count = 2
-  name = "slimtwit-node${count.index +1}"
-  size = "s-1vcpu-1gb"
-
-      connection {
-    user = "root"
-    host = self.ipv4_address
-    type = "ssh"
+  depends_on = [digitalocean_droplet.slimtwit-swarm-manager]
+  image      = "docker-18-04"
+  count      = 2
+  name       = "slimtwit-node${count.index + 1}"
+  size       = "s-1vcpu-1gb"
+  tags       = [digitalocean_tag.taerteform.id]
+  connection {
+    user        = "root"
+    host        = self.ipv4_address
+    type        = "ssh"
     private_key = file(var.pvt_key)
-    timeout = "2m"
+    timeout     = "2m"
   }
 
-  ssh_keys = [ digitalocean_ssh_key.minitwit.fingerprint ]
+  ssh_keys = [digitalocean_ssh_key.minitwit.fingerprint]
 
   provisioner "file" {
-    source = "temp/worker_token"
+    source      = "temp/worker_token"
     destination = "/root/worker_token"
   }
 
   provisioner "remote-exec" {
     inline = [
-    "ufw allow 22/tcp",
+      "ufw allow 22/tcp",
       "ufw allow 2376/tcp",
       "ufw allow 2377/tcp",
       "ufw allow 7946/tcp",
